95a8483fed0c002a23228ae297fd63b7d39a194c
Beko <bekoberk5361@gmail.com>
2024-03-23 19:43:25 +0300

    git commit. Usage: git commit -m “[Showing Coordinates]”

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 def display_coordinates(event):
     my_label['text']=f'x={event.x} y={event.y}'
 my_window = Tk()
+my_window.geometry('750x500')
 my_canvas = Canvas(my_window,width=400,height=400,background='white')
 my_label=Label(bd=4,relief="solid",font="Times 22 bold",bg="white",fg="black")
 my_canvas.bind('<Button-1>',display_coordinates)
